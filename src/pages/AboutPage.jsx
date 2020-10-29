5de709685c42f900e4f3c40d8ef4bc0d697d3858
KuanYuLai <yorklai0523@gmail.com>
2020-10-29 22:22:48 +0800

    Still need to work on profile card in the About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled/macro";
 
 //Components
 import Menu from "../components/Menu";
-
+import OptionBar from "../components/OptionBar";
 //Elements
 import Eddy_Preview from "../static/Eddy_1.jpg";
 import Elvis_Preview from "../static/Elvis_1.jpg";
@@ -11,7 +11,7 @@ import Vaness_Preview from "../static/Vaness_1.jpg";
 
 const Page = styled.div`
   background-color: #083f36;
-  height: 100vh;
+  height: 120vh;
   width: 100vw;
 `;
 
@@ -21,6 +21,8 @@ const Description = styled.div`
   position: relative;
   float: right;
   margin: 20vh 3vw 0 0;
+  display: flex;
+  padding: 0 0 2% 0;
 `;
 
 const Member = styled.div`
@@ -31,10 +33,36 @@ const Member = styled.div`
   margin: 5vh 3vw 0 0;
   display: flex;
   justify-content: space-around;
-  img {
+  padding: 1% 0 4% 0;
+`;
+
+const ProfileImg = styled.div`
+  width: 30%;
+  img{
+    width: 100%;
     height: 40vh;
-    width: 30vw;
-    object-fit: contain;
+    object-fit: cover;
+  }
+`;
+
+const Title = styled.div`
+  width: 45%;
+  h1 {
+    height: 80%;
+    font-size: 5rem;
+    font-weight: 600;
+    letter-spacing: 2.25px;
+    color: #ad9677;
+    margin: 10% auto;
+  }
+`;
+const Content = styled.div`
+  width: 50%;
+  color: #ad9677;
+  text-align; justify;
+  font-size: 100%;
+  a{
+    margin: 0 3%;
   }
 `;
 
@@ -44,27 +72,37 @@ class AboutPage extends Component {
     return (
       <Page>
         <Menu />
+        <OptionBar />
         <Description>
-          <h1> About </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed
-            sapien leo. Praesent bibendum tincidunt diam nec aliquet. Morbi eget
-            ante ac ante eleifend ultrices eu a odio. Aliquam viverra porttitor
-            molestie. Morbi quis lectus non leo aliquet pellentesque. Phasellus
-            suscipit massa quis orci blandit congue. Aenean fringilla efficitur
-            sem, a consectetur eros porttitor lobortis. Nunc tortor metus,
-            feugiat sed sagittis ac, elementum sed augue. Etiam tristique
-            ultrices libero sed mattis. In lacinia condimentum vestibulum. Donec
-            mauris mauris, congue a accumsan non, cursus eu metus. Aliquam
-            facilisis lectus est, id venenatis elit semper efficitur. Integer a
-            odio eu elit sodales dapibus eget et ipsum. Aliquam varius urna in
-            tempor lacinia. In nec tristique velit. Vivamus.
-          </p>
+          <Title>
+            <h1> About </h1>
+          </Title>
+          <Content>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed
+              sapien leo. Praesent bibendum tincidunt diam nec aliquet. Morbi
+              eget ante ac ante eleifend ultrices eu a odio. Aliquam viverra
+              porttitor molestie. Morbi quis lectus non leo aliquet
+              pellentesque. Phasellus suscipit massa quis orci blandit congue.
+              Aenean fringilla efficitur sem, a consectetur eros porttitor
+              lobortis. Nunc tortor metus, feugiat sed sagittis ac, elementum
+              sed augue. Etiam tristique ultrices libero sed mattis.
+              <br />
+              <br />
+              In lacinia condimentum vestibulum. Donec mauris mauris, congue a
+              accumsan non, cursus eu metus. Aliquam facilisis lectus est, id
+              venenatis elit semper efficitur. Integer a odio eu elit sodales
+              dapibus eget et ipsum. Aliquam varius urna in tempor lacinia. In
+              nec tristique velit. Vivamus.
+            </p>
+            <a > Contact </a>
+            <a> sunriental@gmail.com / @sunriental </a>
+          </Content>
         </Description>
         <Member>
-          <img src={Eddy_Preview} alt="Eddy Preview" />
-          <img src={Elvis_Preview} alt="Elvis Preview" />
-          <img src={Vaness_Preview} alt="Vaness Preview" />
+          <ProfileImg> <img style={{objectPosition: "30% 20%"}} src={Elvis_Preview} alt="Elvis Preview" /></ProfileImg>
+          <ProfileImg> <img style={{objectPosition: "30% 20%"}} src={Vaness_Preview} alt="Vaness Preview" /></ProfileImg>
+          <ProfileImg> <img style={{objectPosition: "30% 10%"}}src={Eddy_Preview} alt="Eddy Preview" /></ProfileImg>
         </Member>
       </Page>
     );
